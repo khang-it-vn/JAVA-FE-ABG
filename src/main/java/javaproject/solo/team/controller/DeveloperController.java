a2d10f023ac1bc00b62c9d2aefae2f4d8db0d656
appleghost1101@gmail.com <appleghost1101@gmail.com>
2023-04-02 21:28:49 +0700

    xong chuc nang tim kiem

--- src/main/java/javaproject/solo/team/controller/DeveloperController.java
+++ src/main/java/javaproject/solo/team/controller/DeveloperController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
@@ -17,19 +18,28 @@ public class DeveloperController {
 
     private DocService docService;
 
-    public DeveloperController(DocService docService)
-    {
+    public DeveloperController(DocService docService) {
         super();
         this.docService = docService;
     }
 
     @GetMapping("developer/index")
-    public  String index (Model model, @RequestParam(value = "idDoc", defaultValue = "1") Integer idDoc)
-    {
+    public String index(Model model, @RequestParam(value = "idDoc", defaultValue = "1") Integer idDoc, @RequestParam(value = "keyword", defaultValue = "") String keyword) {
         List<Doc> docs = docService.getAll();
-        model.addAttribute("docs", docs);
+        System.out.println(docs.size());
+        System.out.println(keyword);
+        ArrayList<Doc> docArrayList = new ArrayList<>();
+        if (!keyword.isEmpty()) {
+
+            while ( docs.size() > 0) {
+                if (docs.get(0).getTitle().contains(keyword)) {
+                   docArrayList.add(docs.get(0));
+                }
+                docs.remove(docs.get(0));
+            }
+        }
+        model.addAttribute("docs", docArrayList);
         Doc doc = docService.getById(idDoc);
-        System.out.println(doc.getContent());
         model.addAttribute("doc", doc);
         return "developer/index";
     }
