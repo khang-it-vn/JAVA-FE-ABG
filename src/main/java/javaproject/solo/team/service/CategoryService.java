126732ebc4b7c64453b39e3a912efd8574106140
Khang@22 <nguyenhoangkhangnsc19@gmail.com>
2023-04-02 16:24:19 +0700

    Add use case insert product

--- src/main/java/javaproject/solo/team/service/CategoryService.java
+++ src/main/java/javaproject/solo/team/service/CategoryService.java
@@ -14,4 +14,5 @@ public interface CategoryService {
     Category update(Category category);
 
     void deleteById(Integer id);
+
 }
---
+++ src/main/java/javaproject/solo/team/template_response/ResponseImage.java
@@ -0,0 +1,20 @@
+package javaproject.solo.team.template_response;
+
+public class ResponseImage {
+    private String filename;
+
+    public ResponseImage() {
+    }
+
+    public ResponseImage(String filename) {
+        this.filename = filename;
+    }
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+}
