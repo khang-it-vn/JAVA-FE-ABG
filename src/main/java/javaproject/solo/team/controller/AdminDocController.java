f677a09e657db358c1ec2afa1f99556a1c035266
HuynhThoa <huynhthoa.sugr@gmail.com>
2023-04-02 13:45:12 +0700

    Thoa add admindoc

---
+++ src/main/java/javaproject/solo/team/controller/AdminDocController.java
@@ -0,0 +1,13 @@
+package javaproject.solo.team.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class AdminDocController {
+    @GetMapping("adminDoc/index")
+    public String index() {
+
+        return "adminDoc/index";
+    }
+}
